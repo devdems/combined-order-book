f11d24cdbfc42ad2c0e4e5c6e238706279fa2d81
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-05-31 16:39:34 -0600

    removes server folder and puts express stuff on top level

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/api/hello', (req, res) => {
   res.send('hello');
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
