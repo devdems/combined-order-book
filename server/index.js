1a27391da09aeee7021ad6f69d819dd628ae5302
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-05-31 16:28:57 -0600

    express server set up

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, '../../client/build')));
+
+app.get('/api/hello', (req, res) => {
+  res.send('hello');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on ${port}`);
