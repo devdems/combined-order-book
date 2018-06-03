ac3bf17829ce353b1fc67aea2a01408339f66106
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-06-03 13:50:39 -0600

    switched styling to styled components

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.get('/api/hello', (req, res) => {
-  res.send('hello');
-});
+app.get('/api/hello', (req, res) => res.send('hello'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
