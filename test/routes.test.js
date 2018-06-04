1b3711659271c4a867f332e2f0d6bf573362889e
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-06-03 19:51:51 -0600

    improved tests

--- test/routes.test.js
+++ test/routes.test.js
@@ -5,9 +5,9 @@ const createServer = require('../server.js');
 describe('API Routes', () => {
   var app;
 
-  beforeEach(done => {
+  before(done => {
     app = createServer();
-    return app.listen((err) => {
+    app.listen((err) => {
       if (err) return done(err);
       done();
     });
@@ -15,69 +15,87 @@ describe('API Routes', () => {
 
   describe('poloniex', () => {
     describe('get-order-book', () => {
-      it('should return default order book if missing query params', () => {
-        return request(app)
+      it('should return default order book if missing query params', done => {
+        request(app)
           .get('/api/poloniex/get-order-book')
           .expect(200)
-          .expect(res => {
-            expect(res.body.asks).to.be.an('array');
-            expect(res.body.bids).to.be.an('array');
-          });
+          .then(res => {
+            const { body } = res
+            expect(body.asks.length).to.be.above(0);
+            expect(body.bids.length).to.be.above(0);
+            return done();
+          })
+          .catch(err => done(err));
       });
-      it('should respond with custom order book from standardized query params', () => {
-        return request(app)
+      it('should respond with custom order book from standardized query params', done => {
+        request(app)
           .get('/api/poloniex/get-order-book?market=BTC-XRP')
           .expect(200)
-          .expect(res => {
-            expect(res.body.asks).to.be.an('array');
-            expect(res.body.bids).to.be.an('array');
-          });
+          .then(res => {
+            const { body } = res
+            expect(body.asks.length).to.be.above(0);
+            expect(body.bids.length).to.be.above(0);
+            return done();
+          })
+          .catch(err => done(err));
       });
     });
   });
 
   describe('bittrex', () => {
     describe('get-order-book', () => {
-      it('should return default order book if missing query params', () => {
-        return request(app)
+      it('should return default order book if missing query params', done => {
+        request(app)
           .get('/api/bittrex/get-order-book')
           .expect(200)
-          .expect(res => {
-            expect(res.body.asks).to.be.an('array');
-            expect(res.body.bids).to.be.an('array');
-          });
+          .then(res => {
+            const { body } = res
+            expect(body.asks.length).to.be.above(0);
+            expect(body.bids.length).to.be.above(0);
+            return done();
+          })
+          .catch(err => done(err));
       });
-      it('should respond with custom order book from standardized query params', () => {
-        return request(app)
+      it('should respond with custom order book from standardized query params', done => {
+        request(app)
           .get('/api/bittrex/get-order-book?market=BTC-XRP')
           .expect(200)
-          .expect(res => {
-            expect(res.body.asks).to.be.an('array');
-            expect(res.body.bids).to.be.an('array');
-          });
+          .then(res => {
+            const { body } = res
+            expect(body.asks.length).to.be.above(0);
+            expect(body.bids.length).to.be.above(0);
+            return done();
+          })
+          .catch(err => done(err));
       });
     });
   });
 
   describe('binance', () => {
     describe('get-order-book', () => {
-      it('should return default order book if missing query params', () => {
-        return request(app)
+      it('should return default order book if missing query params', done => {
+        request(app)
           .get('/api/binance/get-order-book')
           .expect(200)
           .expect(res => {
-            expect(res.body.asks).to.be.an('array');
-            expect(res.body.bids).to.be.an('array');
-          });
+            const { body } = res
+            expect(body.asks.length).to.be.above(0);
+            expect(body.bids.length).to.be.above(0);
+            return done();
+          })
+          .catch(err => done(err));
       });
-      it('should respond with custom order book from standardized query params', () => {
-        return request(app)
+      it('should respond with custom order book from standardized query params', done => {
+        request(app)
           .get('/api/binance/get-order-book?market=BTC-XRP')
           .expect(200)
           .expect(res => {
-            expect(res.body.asks).to.be.an('array');
-            expect(res.body.bids).to.be.an('array');
-          });
+            const { body } = res
+            expect(body.asks.length).to.be.above(0);
+            expect(body.bids.length).to.be.above(0);
+            return done();
+          })
+          .catch(err => done(err));
       });
     });
   });
