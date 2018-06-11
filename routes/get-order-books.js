81513d6646896bcecdc3754ccd6faac49e428a68
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-06-10 20:53:03 -0600

    comments and style

--- routes/get-order-books.js
+++ routes/get-order-books.js
@@ -8,18 +8,24 @@ router.get('/', getOrderBooks);
 
 async function getOrderBooks(req, res) {
 
+  // respond with 400 if exchanges not present on request
   if (!req.query.exchanges) {
     return res.status(400).send('No exhanges selected');
   }
 
+  // use the market query param or set default if not present
   const market = req.query.market || 'BTC-ETH';
 
+  // filter out any unsupported exchanges
   const exchanges = req.query.exchanges.split(',').filter(ex =>
     Object.keys(api).includes(ex)
   );
 
   let books = {};
 
+  // request books from each exchange, using util functions to
+  // format market for each API and to standardize the books
+  // upon response
   for (exchange of exchanges) {
     try {
       const translatedMarket = translateMarket[exchange](market);
--- routes/get-supported-exchanges.js
+++ routes/get-supported-exchanges.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const api = require('./api');
 
+// Responds with an array of the exchanges that are
+// supported
+
 router.get('/', getSupportedExchanges);
 
 function getSupportedExchanges(req, res) {
